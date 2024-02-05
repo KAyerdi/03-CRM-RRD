635ae7bed8b6785a9d57ba7bde5639921c238911
Kevin Ayerdi <kevin.ayerdi@hotmail.com>
2024-02-05 13:45:06 -0300

    me fui al gym

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './components/Layout'
 import './index.css'
+import NuevoCliente from './pages/NuevoCliente'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/clientes/nuevo",
+        element: <NuevoCliente/>,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
