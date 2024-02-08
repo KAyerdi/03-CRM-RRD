8258b707bfab7db14d7a53b6c74c35c5adacf81f
Kevin Ayerdi <kevin.ayerdi@hotmail.com>
2024-02-08 17:27:35 -0300

    Agregando funciones

--- src/data/Clientes.js
+++ src/data/Clientes.js
@@ -8,7 +8,14 @@ export async function obtenerClientes(){
 
 export async function agregarCliente(datos) {
     try {
-      const respuesta = await fetch(import.meta.env.VITE_API_URL)
+      const respuesta = await fetch(import.meta.env.VITE_API_URL, {
+        method: 'POST',
+        body: JSON.stringify(datos),
+        headers:{
+          'Content-Type': 'application/json'
+        }
+      })
+      await respuesta.json()
     } catch (error) {
       console.log(error)
     }
--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -1,4 +1,4 @@
-import { Form, useActionData, useNavigate } from 'react-router-dom';
+import { Form, redirect, useActionData, useNavigate } from 'react-router-dom';
 import Error from '../components/Error';
 import Formulario from '../components/Formulario';
 import { agregarCliente } from '../data/Clientes';
@@ -29,8 +29,9 @@ export async function action({ request }) {
     return errores;
   }
 
-  agregarCliente(datos)
-  
+  await agregarCliente(datos)
+
+  return redirect('/')
 }
 
 function NuevoCliente() {
