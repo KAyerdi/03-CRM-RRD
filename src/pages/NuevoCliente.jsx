cc9c908d0e8fc5c41356b59bc1ffd09c91e83fe1
Kevin Ayerdi <kevin.ayerdi@hotmail.com>
2024-02-08 08:50:59 -0300

    Añadiendo json-server al proyecto

--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -1,24 +1,38 @@
-import { Form, useNavigate } from 'react-router-dom';
+import { Form, useActionData, useNavigate } from 'react-router-dom';
+import Error from '../components/Error';
 import Formulario from '../components/Formulario';
 
 
-export async function action({request}) {
-  const formData = await request.formData()
-  
-  const datos = Object.fromEntries(formData)
+export async function action({ request }) {
+  const formData = await request.formData();
+
+  const datos = Object.fromEntries(formData);
+
+  const email = formData.get("email");
 
   //Validacion
-  const errores = []
-  if(Object.values(datos).includes('')) {
-    errores.push('Todos los campos son obligatorios')
+  const errores = [];
+  if (Object.values(datos).includes("")) {
+    errores.push("Todos los campos son obligatorios");
   }
 
-  console.log(errores)
+  let regex = new RegExp(
+    "([!#-'*+/-9=?A-Z^-~-]+(.[!#-'*+/-9=?A-Z^-~-]+)*|\"([]!#-[^-~ \t]|(\\[\t -~]))+\")@([!#-'*+/-9=?A-Z^-~-]+(.[!#-'*+/-9=?A-Z^-~-]+)*|[[\t -Z^-~]*])"
+  );
+  if(!regex.test(email)) {
+    errores.push('El email no es valido')
+  }
+
+  //Retornar datos si hay errores
+  if (Object.keys(errores).length) {
+    return errores;
+  }
 }
 
 function NuevoCliente() {
 
   const navigate = useNavigate()
+  const errores = useActionData()
 
   return (
     <>
@@ -26,24 +40,31 @@ function NuevoCliente() {
       <p className='mt-3'>
         Llena todos los campos para registrar un nuevo cliente
       </p>
+
       <div className='flex justify-end'>
         <button
           className='bg-blue-800 text-white px-3 py-1 font-bold uppercase'
-          onClick={() => navigate("/")}
+          onClick={() => navigate(-1)}
         >
           Volver
         </button>
       </div>
+
       <div className='bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10 mt-20'>
+
+        {errores?.length && errores.map( (error, i) => <Error key={i}>{error}</Error> )}
+
         <Form
           method='post'
+          noValidate
           >
           <Formulario />
+
           <input
             type='submit'
             className='mt-5 w-full bg-blue-800 p-3 uppercase font-bold text-white text-lg'
             value='Registrar Cliente'
-          ></input>
+          />
         </Form>
       </div>
     </>
